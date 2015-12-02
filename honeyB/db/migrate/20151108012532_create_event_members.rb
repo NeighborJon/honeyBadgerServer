c8894da5ff6c1e6e9105b825625fd43b83759438
Jonathan Kendrick <jckv22@mail.missouri.edu>
2015-12-01 18:33:48 -0600

    added checked in variable in event members

--- honeyB/db/migrate/20151108012532_create_event_members.rb
+++ honeyB/db/migrate/20151108012532_create_event_members.rb
@@ -3,6 +3,7 @@ class CreateEventMembers < ActiveRecord::Migration
     create_table :event_members, id: false do |t|
     	t.belongs_to :user, index: true
     	t.belongs_to :event, index: true
+    	t.boolean :checkedin, default: false
     	t.index [:user_id, :event_id], unique: true
     	
     	t.timestamps null: false
