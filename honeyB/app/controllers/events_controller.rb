debfce7cddbcf5e059e9bd1bb089c14beae27db9
Jonathan Kendrick <jckv22@mail.missouri.edu>
2015-12-05 23:18:32 -0600

    fixed search and added exp to friend serializer

--- honeyB/app/controllers/events_controller.rb
+++ honeyB/app/controllers/events_controller.rb
@@ -15,81 +15,171 @@ class EventsController < ApplicationController
     render json: @event
   end
   
+  def is_private(event, user_id)
+  		if event.creator == user_id
+  			return event
+  		end
+		event.members.each do |members|
+			if members.id == user_id
+				return event
+			end
+		end
+		return nil
+  end
+  
   def search
 	eventList = Array.new
 	Event.all.each do |event|
 		eventD = (event.start).strftime("%Y-%m-%d")
+		user = User.find(params[:searcher])
+		is_blocked = false
+		privateSetting = false
+		
+		#THIS CHECKS IF THE CREATOR IS IN THE SEARCHER'S BLOCKED LIST
+		user.blocked.each do |blocked|
+			if blocked.id == event.creator
+				is_blocked = true
+			end
+		end
+		if is_blocked == true
+			next
+		end
+		
+		if event.private
+			privateSetting = true
+		end
+		
 		
 		#searching through 2 filters
 			if params[:eventTitle] != nil && params[:eventCat] != nil
 				if event.title == params[:eventTitle] && event.category == params[:eventCat]
-					eventList << event
+					if privateSetting == true
+								eventList << is_private(event, user.id)
+					else
+						eventList << event
+					end
 				end
 			elsif params[:eventTitle] != nil && params[:eventDate] != nil
 				if event.title == params[:eventTitle] && eventD == params[:eventDate]
-					eventList << event
+					if privateSetting == true
+								eventList << is_private(event, user.id)
+					else
+						eventList << event
+					end
 				end
 			elsif params[:eventTitle] != nil && params[:latMin] != nil && params[:latMax] != nil && params[:longMin] != nil && params[:longMax] != nil
 				if event.title == params[:eventTitle] && event.latitude > ((params[:latMin]).to_f) && event.latitude < ((params[:latMax]).to_f) && event.longitude > ((params[:longMin]).to_f) && event.longitude < ((params[:longMax]).to_f)
-					eventList << event
+					if privateSetting == true
+								eventList << is_private(event, user.id)
+					else
+						eventList << event
+					end
 				end
 			elsif params[:eventCat] != nil && params[:eventDate] != nil
 				if event.category == params[:eventCat] && eventD == params[:eventDate]
-					eventList << event
+					if privateSetting == true
+								eventList << is_private(event, user.id)
+					else
+						eventList << event
+					end
 				end
 			elsif params[:eventCat] != nil && params[:latMin] != nil && params[:latMax] != nil && params[:longMin] != nil && params[:longMax] != nil
 				if event.category == params[:eventCat] && event.latitude > ((params[:latMin]).to_f) && event.latitude < ((params[:latMax]).to_f) && event.longitude > ((params[:longMin]).to_f) && event.longitude < ((params[:longMax]).to_f)
-					eventList << event
+					if privateSetting == true
+								eventList << is_private(event, user.id)
+					else
+						eventList << event
+					end
 				end
 			elsif params[:eventDate] != nil && params[:latMin] != nil && params[:latMax] != nil && params[:longMin] != nil && params[:longMax] != nil
 				if eventD == params[:eventDate] && event.latitude > ((params[:latMin]).to_f) && event.latitude < ((params[:latMax]).to_f) && event.longitude > ((params[:longMin]).to_f) && event.longitude < ((params[:longMax]).to_f)
-					eventList << event
+					if privateSetting == true
+								eventList << is_private(event, user.id)
+					else
+						eventList << event
+					end
 				end
 			end
 			
 		#searching through 3 filters
 			if params[:eventTitle] != nil && params[:eventCat] != nil && params[:eventDate] != nil
 				if event.title == params[:eventTitle] && event.category == params[:eventCat] && eventD == params[:eventDate]
-					eventList << event
+					if privateSetting == true
+								eventList << is_private(event, user.id)
+					else
+						eventList << event
+					end
 				end
 			elsif params[:eventTitle] != nil && params[:eventCat] != nil && params[:latMax] != nil && params[:longMin] != nil && params[:longMax] != nil
 				if event.title == params[:eventTitle] && event.category == params[:eventCat] && event.latitude > ((params[:latMin]).to_f) && event.latitude < ((params[:latMax]).to_f) && event.longitude > ((params[:longMin]).to_f) && event.longitude < ((params[:longMax]).to_f)
-					eventList << event
+					if privateSetting == true
+								eventList << is_private(event, user.id)
+					else
+						eventList << event
+					end
 				end
 			elsif params[:eventTitle] != nil && params[:eventDate] != nil && params[:latMax] != nil && params[:longMin] != nil && params[:longMax] != nil
 				if event.title == params[:eventTitle] && eventD == params[:eventDate] && event.latitude > ((params[:latMin]).to_f) && event.latitude < ((params[:latMax]).to_f) && event.longitude > ((params[:longMin]).to_f) && event.longitude < ((params[:longMax]).to_f)
-					eventList << event
+					if privateSetting == true
+								eventList << is_private(event, user.id)
+					else
+						eventList << event
+					end
 				end
 			elsif params[:eventCat] != nil && params[:eventDate] != nil && params[:latMax] != nil && params[:longMin] != nil && params[:longMax] != nil
 				if event.category == params[:eventCat] && eventD == params[:eventDate] && event.latitude > ((params[:latMin]).to_f) && event.latitude < ((params[:latMax]).to_f) && event.longitude > ((params[:longMin]).to_f) && event.longitude < ((params[:longMax]).to_f)
-					eventList << event
+					if privateSetting == true
+								eventList << is_private(event, user.id)
+					else
+						eventList << event
+					end
 				end
 			end
 			
 		#searching through all filters
 			if params[:eventTitle] != nil && params[:eventCat] != nil && params[:eventDate] != nil && params[:eventDate] != nil && params[:latMax] != nil && params[:longMin] != nil && params[:longMax] != nil
 				if event.title == params[:eventTitle] && event.category == params[:eventCat] && eventD == params[:eventDate] && event.latitude > ((params[:latMin]).to_f) && event.latitude < ((params[:latMax]).to_f) && event.longitude > ((params[:longMin]).to_f) && event.longitude < ((params[:longMax]).to_f)
-					eventList << event
+					if privateSetting == true
+								eventList << is_private(event, user.id)
+					else
+						eventList << event
+					end
 				end
 			end
 			
 		#searching through a single filter
 			if params[:eventTitle] != nil && params[:eventCat] == nil && params[:eventDate] == nil && params[:eventDate] == nil && params[:latMax] == nil && params[:longMin] == nil && params[:longMax] == nil
 			if event.title == params[:eventTitle]
-					eventList << event
+					if privateSetting == true
+								eventList << is_private(event, user.id)
+					else
+						eventList << event
+					end
 				end
 			elsif params[:eventCat] != nil && params[:eventTitle] == nil && params[:eventDate] == nil && params[:latMax] == nil && params[:longMin] == nil && params[:longMax] == nil
 				if event.category == params[:eventCat]
-					eventList << event
+					if privateSetting == true
+								eventList << is_private(event, user.id)
+					else
+						eventList << event
+					end
 				end
 
 			elsif params[:eventDate] != nil && params[:eventTitle] == nil && params[:eventCat] == nil && params[:latMin] == nil && params[:latMax] == nil && params[:longMin] == nil && params[:longMax] == nil
 				if eventD == params[:eventDate]
-					eventList << event
+					if privateSetting == true
+								eventList << is_private(event, user.id)
+					else
+						eventList << event
+					end
 				end
 			elsif params[:latMin] != nil && params[:latMax] != nil && params[:longMin] != nil && params[:longMax] != nil && params[:eventDate] == nil && params[:eventTitle] == nil && params[:eventCat] == nil
 				if event.latitude > ((params[:latMin]).to_f) && event.latitude < ((params[:latMax]).to_f) && event.longitude > ((params[:longMin]).to_f) && event.longitude < ((params[:longMax]).to_f)
-					eventList << event
+					if privateSetting == true
+								eventList << is_private(event, user.id)
+					else
+						eventList << event
+					end
 				end
 			end
 				
@@ -100,12 +190,35 @@ class EventsController < ApplicationController
   end
   
   def mapEvents
-
   	mapList = Array.new 
   	Event.all.each do |event|
+  	
+	is_blocked = false
+	privateSetting = false
+	user = User.find(params[:searcher])
+	#THIS CHECKS IF THE CREATOR IS IN THE SEARCHER'S BLOCKED LIST
+	user.blocked.each do |blocked|
+		if blocked.id == event.creator
+			is_blocked = true
+		end
+	end
+	if is_blocked == true
+		next
+	end
+
+
+	if event.private
+		privateSetting = true
+	end
+
+
   		if event.longitude > ((params[:longMin]).to_f) && event.longitude < ((params[:longMax]).to_f)
  				if event.latitude > ((params[:latMin]).to_f) && event.latitude < ((params[:latMax]).to_f)
-  						mapList << event
+					if privateSetting == true
+						eventList << is_private(event, user.id)
+					else
+						eventList << event
+					end
   				end	
 		end
   		
--- honeyB/app/serializers/friend_serializer.rb
+++ honeyB/app/serializers/friend_serializer.rb
@@ -1,3 +1,3 @@
 class FriendSerializer < ActiveModel::Serializer
-  attributes :id, :fName, :lName, :avatar_path
+  attributes :id, :fName, :lName, :avatar_path, :educationExp, :shenaniganExp, :fitnessExp, :philanthropyExp
 end
